28b4647bd00e951394d5142a0cabc0bca4bfab40
Jakub Jucha <jakubjucha800@gmail.com>
2025-03-26 18:32:19 +0100

    add singout on server

---
+++ Pawnshop.Application/UsersApplication/Commands/Logout/LogoutCommand.cs
@@ -0,0 +1,15 @@
+﻿using Pawnshop.Application.Base;
+using Pawnshop.Application.UsersApplication.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pawnshop.Application.UsersApplication.Commands.Logout
+{
+    public class LogoutCommand : BaseCommand<LogoutResponse>
+    {
+        public string RefreshToken { get; set; }
+    }
+}
