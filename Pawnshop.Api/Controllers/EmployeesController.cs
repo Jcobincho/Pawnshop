5c689f1b71731e2e1fe6019ad7095411d58ad99e
Jakub Jucha <jakubjucha800@gmail.com>
2025-04-02 11:08:28 +0200

    Add controllers do empoyees management

---
+++ Pawnshop.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pawnshop.Application.Base;
+using Pawnshop.Application.EmployeesApplication.Commands.AddEmployee;
+using Pawnshop.Application.EmployeesApplication.Commands.DeleteEmployee;
+using Pawnshop.Application.EmployeesApplication.Commands.EditEmployee;
+
+namespace Pawnshop.Api.Controllers
+{
+    [Route("[controller]")]
+    public class EmployeesController : BaseController<AddEmployeeCommand, EditEmployeeCommand, DeleteEmployeeCommand, BaseQuery>
+    {
+        [NonAction]
+        public override Task<IActionResult> GetAsync([FromQuery] BaseQuery data, CancellationToken cancellation)
+        {
+            return base.GetAsync(data, cancellation);
+        }
+    }
+}
