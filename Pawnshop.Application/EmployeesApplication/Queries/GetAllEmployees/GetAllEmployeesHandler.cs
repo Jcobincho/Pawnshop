81055c892b2390b644093c5c55f56dc991e36c92
Jakub Jucha <jakubjucha800@gmail.com>
2025-04-02 11:50:48 +0200

    Add get all empolyees controller

---
+++ Pawnshop.Application/EmployeesApplication/Queries/GetAllEmployees/GetAllEmployeesHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Pawnshop.Application.EmployeesApplication.Interfaces;
+using Pawnshop.Application.EmployeesApplication.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pawnshop.Application.EmployeesApplication.Queries.GetAllEmployees
+{
+    public sealed class GetAllEmployeesHandler : IRequestHandler<GetAllEmployeesQuery, GetAllEmployeesResponse>
+    {
+        private readonly IEmployeesQueryService _employeesQueryService;
+
+        public GetAllEmployeesHandler(IEmployeesQueryService employeesQueryService)
+        {
+            _employeesQueryService = employeesQueryService;
+        }
+
+        public async Task<GetAllEmployeesResponse> Handle(GetAllEmployeesQuery request, CancellationToken cancellationToken)
+        {
+            var allEmployees = await _employeesQueryService.GetAllEmployeesAsDtoAsync(cancellationToken);
+
+            return new GetAllEmployeesResponse()
+            {
+                AllEmployeesList = await
+            
+        }
+    }
+}
