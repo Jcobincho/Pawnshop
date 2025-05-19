d186606fe0663c757493ce62589eb68b35cb83e7
Jakub Jucha <jakubjucha800@gmail.com>
2025-05-19 22:20:31 +0200

    add controller to manage item history

---
+++ Pawnshop.Api/Controllers/ItemHistoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pawnshop.Application.Base;
+using Pawnshop.Application.ItemHistoriesApplication.Commands.AddItemHistory;
+using Pawnshop.Application.ItemHistoriesApplication.Commands.DeleteItemHistory;
+using Pawnshop.Application.ItemHistoriesApplication.Commands.UpdateItemHistory;
+
+namespace Pawnshop.Api.Controllers
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class ItemHistoriesController : BaseController<AddItemHistoryCommand, UpdateItemHistoryCommand, DeleteItemHistoryCommand, BaseQuery>
+    {
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public override Task<IActionResult> GetAsync([FromQuery] BaseQuery data, CancellationToken cancellation)
+        {
+            return base.GetAsync(data, cancellation);
+        }
+    }
+}
